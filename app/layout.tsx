4a28d60ed923adba2e4da718213dc9b2ea7ced2f
tanvirahammed308 <imtanvir.email@gmail.com>
2024-01-10 21:16:13 +0600

    f2

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
       
     <html lang="en">
       <body className={inter.className}>
@@ -37,6 +37,6 @@ export default function RootLayout({
           </ThemeProvider>
         </body>
     </html>
-    </ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
+    </ClerkProvider >
   )
 }
